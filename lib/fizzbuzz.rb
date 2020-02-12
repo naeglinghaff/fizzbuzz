e0b98832aa4647381cf8a64bcb2aec7253d8df54
naeglinghaff <kate.o.haffenden@gmail.com>
2020-02-12 16:24:12 +0000

    Refactored fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(number)
-    if number == 3
-      return "fizz"
-    elsif number == 5
-      return "buzz"
-    elsif number == 15
+    if number % 3 == 0 && number % 5 == 0
       return "fizzbuzz"
+    elsif number % 3 == 0
+      return "fizz"
+    elsif number % 5 == 0
+      return "buzz"
     else
       return number
     end
